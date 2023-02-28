75ee6ec765ee6512120a3da17335b275dc0cfc90
runVprog <perdongem7@gmail.com>
2023-02-28 09:04:23 +0800

    updated

--- gitadd.sh
+++ gitadd.sh
@@ -1,7 +1,4 @@
 # Git add
 # Usage: bash gitadd.sh .
-git add .
-git config --global user.name "runVprog"
-git commit -m "Updated index"
-git push
+{ git add . ; git commit -m "Updated index"; git push }
 
